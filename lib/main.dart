529f645972d8c62a3ed914b67c10e7a779a97aea
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-03-13 18:14:52 +0000

    informacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Proveedor {
+  int id;
+  String proveedor;
+  String nombre;
+  String apellido;
+  String cargo;
+  double salario;
+  DateTime fechaContratacion;
+  String telefono;
+
+  Proveedor({
+    required this.id,
+    required this.proveedor,
+    required this.nombre,
+    required this.apellido,
+    required this.cargo,
+    required this.salario,
+    required this.fechaContratacion,
+    required this.telefono,
+  });
+}
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Proveedores')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildProveedorContainer(
+                Proveedor(
+                  id: 1,
+                  proveedor: 'Proveedor A',
+                  nombre: 'Juan',
+                  apellido: 'Pérez',
+                  cargo: 'Gerente',
+                  salario: 5000.0,
+                  fechaContratacion: DateTime(2023, 1, 15),
+                  telefono: '123-456-7890',
+                ),
+                Colors.blue[100]!,
+              ),
+              SizedBox(height: 20), // Espacio entre los contenedores
+              _buildProveedorContainer(
+                Proveedor(
+                  id: 2,
+                  proveedor: 'Proveedor B',
+                  nombre: 'María',
+                  apellido: 'Gómez',
+                  cargo: 'Analista',
+                  salario: 3500.0,
+                  fechaContratacion: DateTime(2023, 3, 10),
+                  telefono: '987-654-3210',
+                ),
+                Colors.green[100]!,
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+  static Widget _buildProveedorContainer(Proveedor proveedor, Color color) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('ID: ${proveedor.id}'),
+              Text('Proveedor: ${proveedor.proveedor}'),
+              Text('Nombre: ${proveedor.nombre} ${proveedor.apellido}'),
+              Text('Cargo: ${proveedor.cargo}'),
+              Text('Salario: \$${proveedor.salario}'),
+              Text(
+                  'Fecha de Contratación: ${proveedor.fechaContratacion.toLocal()}'),
+              Text('Teléfono: ${proveedor.telefono}'),
+            ],
+          ),
+        ],
       ),
     );
   }
